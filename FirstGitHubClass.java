aede264e06481c59c0b751086ffe7cab791e6a96
youyandong123 <45580331+youyandong123@users.noreply.github.com>
2019-04-13 10:34:03 +0800

    优化代码
    一点点修改

--- FirstGitHubClass.java
+++ FirstGitHubClass.java
@@ -3,7 +3,7 @@ package first;
 public class FirstGitHubClass {
 
 	public static void main(String[] args) {
-		System.out.println("Hello, GitHub !");
+		System.out.println("Hello, GitHub ! Again ！！");
 
 	}
 
